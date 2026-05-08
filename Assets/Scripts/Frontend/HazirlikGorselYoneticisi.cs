0b9605a396f053a5f7d31a69d64e7a6de9777bf4
rumeysanur <rumeysanrr0@gmail.com>
2026-05-08 22:09:00 +0300

    Backend frontend dosya ayrımları yapıldı.Çigköfte butonu eklendi

---
+++ Assets/Scripts/Frontend/HazirlikGorselYoneticisi.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+public class HazirlikGorselYoneticisi : MonoBehaviour
+{
+    [Header("Tezgah Görselleri")]
+    public GameObject LavasGorsel;
+    public GameObject CigkofteGorsel;
+    public GameObject MarulGorsel;
+    public GameObject AcısosGorsel;
+    public GameObject NareksisiGorsel;
+    public GameObject DoritosGorsel;
+
+    private void OnEnable()
+    {
+        // Backend'i dinlemeye başlıyoruz (Abone oluyoruz)
+        HazirlikYoneticisi.OnMalzemeEklendi += GorseliAktifEt;
+        HazirlikYoneticisi.OnTezgahTemizlendi += TumGorselleriKapat;
+    }
+
+    private void OnDisable()
+    {
+        // Abonelikten çıkıyoruz
+        HazirlikYoneticisi.OnMalzemeEklendi -= GorseliAktifEt;
+        HazirlikYoneticisi.OnTezgahTemizlendi -= TumGorselleriKapat;
+    }
+
+    private void GorseliAktifEt(MalzemeSO malzeme)
+    {
+        string ad = malzeme.ekrandaGozukenAd;
+
+        if (ad == "Lavaş") LavasGorsel.SetActive(true);
+        else if (ad == "Çiğköfte") CigkofteGorsel.SetActive(true);
+        else if (ad == "Marul") MarulGorsel.SetActive(true);
+        else if (ad == "Acı Sos") AcısosGorsel.SetActive(true);
+        else if (ad == "Nar Ekşisi") NareksisiGorsel.SetActive(true);
+        else if (ad == "Doritos") DoritosGorsel.SetActive(true);
+    }
+
+    private void TumGorselleriKapat()
+    {
+        LavasGorsel.SetActive(false);
+        CigkofteGorsel.SetActive(false);
+        MarulGorsel.SetActive(false);
+        AcısosGorsel.SetActive(false);
+        NareksisiGorsel.SetActive(false);
+        DoritosGorsel.SetActive(false);
+    }
+}
